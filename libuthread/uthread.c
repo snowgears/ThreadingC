82f9dea3beaf0d79b7b468bd2c8d65d6a23ebbc2
Tanner Nelson Embry <awesomete@gmail.com>
2017-04-27 12:43:35 -0700

    write free method and work on defining more rigid bounds for queue_create()

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -26,10 +26,38 @@ typedef struct {
 
 } thread_control_block;
 
+/* Free all of the memory of a given thread control block */
+void free_thread(struct thread_control_block* tcb)
+{
+        if(tcb == NULL){
+                return;
+        }
+        //only free the context if it has allocated memory
+        if(tcb->context != NULL){
+                free(tcb->context);
+        }
+        free(tcb);
+}
+
+/* Callback function that finds a certain item according to its value */
+static int find_thread(queue_t q, void *data, void *arg)
+{
+        //TODO this will be a function to find a thread in the queue by its id
+        //will return a pointer to the TCB and use queue_iterate to solve
+	thread_control_block *thread = (int*)data;
+	int match = (intptr_t)arg;
+
+	if (*a == match)
+	return 1;
+
+	return 0;
+}
+
 
 void uthread_yield(void)
 {
 	//from what I understand this involves swapping the thread context of current TCB with context of next TCB in the queue
+        //swap contexts of current thread and next active thread in queue
 
 }
 
@@ -40,23 +68,28 @@ uthread_t uthread_self(void)
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	// TODO: only save main TCB on first call
-	// Main TCB
-	thread_control_block* mainTCB = (thread_control_block*) malloc(sizeof(thread_control_block));
-	uthread_ctx_t* mainCXT = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
-	mainTCB->context = mainCXT;
-	mainTCB->id = thread_count;
-	mainTCB->state = STATE_RUNNING;
-	
-	if(thread_queue == NULL){
+        thread_control_block* currTCB;
+
+        //if this is the first call to create_thread(), initialze queue and save TCB of main to current TCB
+        if(thread_queue == NULL){
 		thread_queue = (queue_t)queue_create(); //will have a queue of thread control blocks
+
+                currTCB = (thread_control_block*) malloc(sizeof(thread_control_block));
+        	uthread_ctx_t* mainCXT = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
+        	currTCB->context = mainCXT;
+        	currTCB->id = thread_count;
+        	currTCB->state = STATE_RUNNING;
 	}
-	
+        //if the queue has been initialized, there is a current running thread in the queue
+        else{
+                //assign currentTCB from the current running thread
+
+        }
+
+        //initialize a new thread to execute the provided function with the given arguments
 	uthread_t tid = ++thread_count;
 	thread_control_block* tcb = (thread_control_block*) malloc(sizeof(thread_control_block));
 	uthread_ctx_t* cxt = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
-	
-
 
 	void* stack = uthread_ctx_alloc_stack(); //not sure if this is necessary
 	int init_failed = uthread_ctx_init(cxt, stack, func, arg);
@@ -64,14 +97,14 @@ int uthread_create(uthread_func_t func, void *arg)
 	if(init_failed == -1){
 		 //TODO: the intializattion failed, not sure what to do from here
 	}
-	
+
 	// Set thread control block
 	tcb->context = cxt;
 	tcb->id = tid;
 	tcb->state = STATE_READY;
 
 	queue_enqueue(thread_queue, (void*) tcb);
-	
+
 	// context switch [running the next thread]
 	uthread_ctx_switch(mainCXT, cxt);
 
@@ -90,4 +123,3 @@ int uthread_join(uthread_t tid, int *retval)
 	//TODO: PHASE 3
 	return 0;
 }
-
